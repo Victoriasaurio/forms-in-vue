1473ec6ccd52beceec32006466bdc26c4f31584e
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-21 14:43:19 -0600

    Section 09: Formulario - Anotaciones sobre los formualrios en Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="col-md-6">
+      <h1>Formulario</h1>
+      <form>
+        <div class="form-group">
+          <label for="correo">Nombre</label>
+          <input type="text" class="form-control" placeholder="Nombre"
+                  v-model.trim="usuario.nombre">
+        </div> 
+        <div class="form-group">
+          <label for="correo">Correo Electrónico</label>
+          <input type="email" class="form-control" placeholder="Correo Electrónico"
+                  v-model.lazy="usuario.correo"> <!--Actualiza el campo hasta que salga del input-->
+        </div>
+        <div class="form-group">
+          <label for="clave">Contraseña</label>
+          <input type="password" class="form-control" placeholder="Contraseña"
+                  v-model="usuario.clave">
+        </div>
+        <div class="form-group">
+          <label for="clave">Edad</label>
+          <input type="text" class="form-control" placeholder="Edad"
+                  v-model.number="usuario.edad">
+        </div>
+        <div class="form-group">
+          <label for="pais">País</label>
+          <select class="form-control" v-model="usuario.pais">
+            <option v-for="pais in paises" :key="pais">{{pais}}</option>
+          </select>
+        </div>
+        
+        <div class="form-group">
+          <label for="mensaje">Mensaje</label>
+          <textarea class="form-control" rows="3"
+                    v-model="usuario.mensaje"></textarea>
+        </div>
+
+        <fieldset class="form-group">
+          <legend>Genero</legend>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios"  
+                    value="hombre" checked v-model="usuario.genero">
+              Hombre
+            </label>
+          </div>
+          <div class="form-check">
+          <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios"  
+                    value="mujer" v-model="usuario.genero">
+              Mujer
+            </label>
+          </div>
+          
+          </fieldset>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="checkbox" class="form-check-input" value="acepto"
+              v-model="usuario.condiciones">
+              Acepto las condiciones
+            </label>
+          </div>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="checkbox" class="form-check-input" value="newsletter"
+              v-model="usuario.condiciones">
+              Recibir la newsletter
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary"
+                  @click.prevent="enviarFormulario">Enviar</button>
+      </form>
+    </div>
+    <div class="resultados col-md-6" v-if="mostrar">
+      <h1>Resultados</h1>
+      <p><strong>Nombre: </strong> {{usuario.nombre}} - {{usuario.nombre.length}}</p>
+      <p><strong>Correo Electrónico: </strong>{{usuario.correo}}</p>
+      <p><strong>Contraseña: </strong>{{usuario.clave}}</p>
+      <p><strong>Edad: </strong>{{usuario.edad}} - {{ typeof usuario.edad }}</p>
+      <p><strong>País: </strong>{{usuario.pais}}</p>
+      <p class="mensaje"><strong>Mensaje: </strong>{{usuario.mensaje}}</p>
+      <p><strong>Genero: </strong>{{usuario.genero}}</p>
+      <p><strong>Condiciones: </strong>{{usuario.condiciones}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      usuario: { //Agrupa los valores en un objeto.
+        nombre : '',
+        correo : '',
+        clave : '',
+        edad : '',
+        pais : 'México',
+        mensaje: '',
+        genero: 'mujer',
+        condiciones: []
+      },
+      mostrar : false, //Oculta el formulario
+      paises : ['Argentina','Bolivia','Brasil','Chile','Colombia','Ecuador','España','México','Paraguay','Uruguay','Venezuela']
+
+    }
+  },
+  methods:{
+    enviarFormulario(){
+      this.mostrar = true //Muestra el formulario
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .mensaje{ /*Respeta el mensaje cuando haya líneas diferentes*/
+    white-space: pre;
+  }
 </style>
